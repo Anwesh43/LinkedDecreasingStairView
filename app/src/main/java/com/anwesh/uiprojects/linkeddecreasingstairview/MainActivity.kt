fac7ea6a0c337aea1857f523fa47f11e19276750
Anwesh43 <anweshthecool0@gmail.com>
2019-03-17 13:35:23 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkeddecreasingstairview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkeddecreasingstairview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkeddecreasingstairview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.decreasingstairview.DecreasingStairView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        DecreasingStairView.create(this)
     }
 }
