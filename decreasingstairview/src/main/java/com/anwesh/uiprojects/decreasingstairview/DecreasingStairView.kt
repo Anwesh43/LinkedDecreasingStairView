70dd7563cb2bbc8ab048a143291ee6f9c1f522f7
Anwesh43 <anweshthecool0@gmail.com>
2019-03-17 13:34:21 +0530

    adding static create method

--- decreasingstairview/src/main/java/com/anwesh/uiprojects/decreasingstairview/DecreasingStairView.kt
+++ decreasingstairview/src/main/java/com/anwesh/uiprojects/decreasingstairview/DecreasingStairView.kt
@@ -208,4 +208,13 @@ class DecreasingStairView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : DecreasingStairView {
+            val view : DecreasingStairView = DecreasingStairView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
