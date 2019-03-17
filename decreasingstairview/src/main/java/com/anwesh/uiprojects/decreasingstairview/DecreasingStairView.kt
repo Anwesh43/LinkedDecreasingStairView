a8f14ea713295923540fbbea4184e2c80de9547e
Anwesh43 <anweshthecool0@gmail.com>
2019-03-17 13:26:40 +0530

    adding DecreasingStair class

--- decreasingstairview/src/main/java/com/anwesh/uiprojects/decreasingstairview/DecreasingStairView.kt
+++ decreasingstairview/src/main/java/com/anwesh/uiprojects/decreasingstairview/DecreasingStairView.kt
@@ -162,4 +162,27 @@ class DecreasingStairView(ctx : Context) : View(ctx) {
             return this
         }
     }
+
+    data class DecreasingStair(var i : Int) {
+
+        private var curr : DSNode = DSNode(0)
+        private var dir : Int = 1
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            curr.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            curr.update {i, scl ->
+                curr = curr.getNext(dir) {
+                    dir *= -1
+                }
+                cb(i, scl)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            curr.startUpdating(cb)
+        }
+    }
 }
